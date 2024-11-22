c9e98097910021381b450be77cfcb88f684420d7
Juan-Script <juanreig197@gmail.com>
2024-11-22 09:28:35 +0100

    Limpiados comentarios en HeroHome

--- components/shared/HeroHome.tsx
+++ components/shared/HeroHome.tsx
@@ -4,7 +4,6 @@ import { Flex, Text } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import React from 'react'
 
-// Definimos el motion para Flex y Text
 const MotionFlex = motion(Flex)
 const MotionText = motion(Text)
 
@@ -31,16 +30,16 @@ export default function HeroHome() {
                 top={{ base: "100px", sm: "150px", md: "200px" }}
                 right={{ base: "60px", sm: "50px", md: "100px", lg: "300px" }}
                 maxW={{ base: "300px", sm: "350px", md: "400px" }}
-                initial={{ opacity: 0, x: 100 }} // Posición inicial, sale de la derecha
-                animate={{ opacity: 1, x: 0 }} // Animación a la posición final
-                transition={{ duration: 1 }} // Duración de la animación
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1 }}
             >
                 <MotionText
                     fontSize={{ base: "28px", sm: "32px", md: "36px", lg: "40px" }}
                     fontWeight={700}
-                    initial={{ opacity: 0, x: -100 }} // Empieza desde la izquierda
-                    animate={{ opacity: 1, x: 0 }} // Llega al centro con opacidad completa
-                    transition={{ duration: 1 }} // La misma duración que el contenedor
+                    initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1 }}
                 >
                     Cirugía responsable
                 </MotionText>
@@ -48,9 +47,9 @@ export default function HeroHome() {
                 <MotionText
                     fontSize={{ base: "18px", sm: "15px", md: "16px" }}
                     fontWeight={400}
-                    initial={{ opacity: 0, x: 100 }} // Empieza desde la derecha
-                    animate={{ opacity: 1, x: 0 }} // Mismo efecto
-                    transition={{ duration: 1, delay: 0.5 }} // Un poco de retraso para que aparezca después del título
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 0.5 }}
                 >
                     Apuesto por una cirugía responsable, que no cree en el mercado de las apariencias sino en mejorar la belleza natural e intrínseca de cada individuo.
                 </MotionText>
