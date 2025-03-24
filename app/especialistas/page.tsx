5cd3cc1019ca8fb127ee032f98b76ac4cb888dd3
Juan-Script <juanreig197@gmail.com>
2025-03-24 09:10:27 +0100

    fix: deploy arreglado quitando generateMetadata de especialistas

--- app/especialistas/page.tsx
+++ app/especialistas/page.tsx
@@ -7,28 +7,6 @@ import { Hero } from "@/components/shared/Hero";
 import Navbar from "@/components/shared/Navbar";
 import { Flex, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import { Metadata } from "next";
-
-export async function generateMetadata(): Promise<Metadata> {
-    const TITLE = "Clínica DRJM"
-    const DESCRIPTION = 'Clínica DRJM - Centro médico especializado en cirugía plástica y medicina estética. Ofrecemos tratamientos personalizados con los más altos estándares de calidad y seguridad para nuestros pacientes.'
-    return {
-        title: TITLE,
-        description: DESCRIPTION,
-        metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
-        alternates: {
-            canonical: process.env.NEXT_PUBLIC_BASE_URL,
-        },
-        robots: {
-            index: true,
-            follow: true,
-            googleBot: {
-                index: true,
-                follow: true,
-            },
-        },
-    }
-  }
 
 const MotionFlex = motion(Flex);
 const MotionText = motion(Text);
